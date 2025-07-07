54d694f53d404ae1bbc95a41846fd2ab96d92c27
rgroemmer <raphael.groemmer@stackit.cloud>
2025-07-08 01:10:08 +0200

    Add XDG envvars

--- modules/home-manager/desktop/hyprland/default.nix
+++ modules/home-manager/desktop/hyprland/default.nix
@@ -56,9 +56,17 @@ in {
       ];
     };
 
-    # Make nix-path available for all tools
+    # environment.d defines environment variables for the user session, beyond shell level.
+    # It is processed by `systemd --user`, basically after login.
+    # - Adds the nix-store-path to the `PATH` globally for user applications.
+    # - Adds XDG_DATA_DIRS globally to allign GTK themes and config dirs session wide.
     xdg.configFile."environment.d/envvars.conf".text = ''
       PATH="$HOME/.nix-profile/bin:$PATH"
+      XDG_DATA_DIRS=${concatStringsSep ":" [
+        "${config.home.homeDirectory}/.nix-profile/share"
+        "/usr/share"
+        "/nix/var/nix/profiles/default/share"
+      ]}
     '';
   };
 }
