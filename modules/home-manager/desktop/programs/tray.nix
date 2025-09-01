18cd62d924d059d8b6d706f89f2808d972d634d4
rgroemmer <github@rapsn.me>
2025-09-01 22:19:12 +0200

    Reactivate tray

--- modules/home-manager/desktop/programs/tray.nix
+++ modules/home-manager/desktop/programs/tray.nix
@@ -1,7 +1,6 @@
 {
-  # TODO:(zion): check if needed?
   services = {
-    # network-manager-applet.enable = true;
-    # blueman-applet.enable = true;
+    network-manager-applet.enable = true;
+    blueman-applet.enable = true;
   };
 }
