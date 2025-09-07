ba8daef858cb41bb4ddcf117ff7bdc1fd208e732
Raphael Grömmer <38243306+RAPSNX@users.noreply.github.com>
2025-09-07 17:08:57 +0200

    chore(bug): use lazygit tabs (#35)

--- modules/home-manager/cli/programs/lazygit.nix
+++ modules/home-manager/cli/programs/lazygit.nix
@@ -10,12 +10,6 @@
           pager = "${config.programs.git.delta.package}/bin/delta --dark --paging=never";
         };
       };
-      os = {
-        editPreset = "nvim-remote";
-        edit = ''[ -z "$NVIM" ] && (nvim -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}})'';
-        editAtLine = ''[ -z "$NVIM" ] && (nvim +{{line}} -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" &&  nvim --server "$NVIM" --remote {{filename}} && nvim --server "$NVIM" --remote-send ":{{line}}<CR>")'';
-        openDirInEditor = ''[ -z "$NVIM" ] && (nvim -- {{dir}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{dir}})'';
-      };
     };
   };
 }
