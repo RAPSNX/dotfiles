de3eb051df9804e25374fa1b4a986b1635e2b4c1
RAPSNX <github@rapsn.me>
2025-12-03 23:24:32 +0100

    remove stuff

--- modules/home-manager/desktop/hyprland/config/workspaces.nix
+++ modules/home-manager/desktop/hyprland/config/workspaces.nix
@@ -13,6 +13,9 @@
     "bind = SUPER,4, workspace, 4"
     "bind = SUPER,5, workspace, 5"
     "bind = SUPER,6, workspace, 6"
+    "bind = SUPER,7, workspace, 7"
+    "bind = SUPER,8, workspace, 8"
+    "bind = SUPER,9, workspace, 9"
 
     # Workspace window movement
     "bind = ALT,1, movetoworkspace, 1"
