e0b6fa2ba7bfc2aca2c48641b1ec34994beb6227
RAPSNX <github@rapsn.me>
2025-12-16 11:40:31 +0100

    Fix issues

--- modules/home/desktops/hyprland/settings/autostart.nix
+++ modules/home/desktops/hyprland/settings/autostart.nix
@@ -10,6 +10,7 @@ in
 {
   wayland.windowManager.hyprland.settings = lib.mkIf cfg.enable {
     exec-once = [
+      # TODO: Move to own option, build function for uwsm
       "uwsm app -- ${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
       "[ workspace special:scratchy silent ] alacritty -t scratchy"
       "sleep 5 && hyprctl dispatch closewindow class:ZSTray"
