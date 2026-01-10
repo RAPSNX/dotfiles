b4219adaa4fd7a144177b34791746697a9dab858
RAPSNX <github@rapsn.me>
2026-01-10 19:41:09 +0100

    fix thunar, update flake

--- modules/nixos/roles/desktop/explorer.nix
+++ modules/nixos/roles/desktop/explorer.nix
@@ -4,21 +4,28 @@
   pkgs,
   ...
 }:
-with lib; let
+with lib;
+let
   cfg = config.hostConfiguration.roles.desktop;
-in {
+in
+{
   options.hostConfiguration.roles.desktop = mkEnableOption "Enable hyprland and desktop features.";
 
   config = mkIf cfg {
     programs = {
       thunar = {
         enable = true;
-        plugins = with pkgs.xfce; [
+        plugins = with pkgs; [
           thunar-archive-plugin
           thunar-volman
         ];
       };
+      xfconf.enable = true;
     };
+
+    environment.systemPackages = [
+      pkgs.file-roller
+    ];
 
     services = {
       gvfs.enable = true; # virtual filesystems
