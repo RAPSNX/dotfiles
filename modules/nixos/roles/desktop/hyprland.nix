d326c448571f39a9409c473410522b5f4e11630e
RAPSNX <mail@rapsn.me>
2026-06-24 00:40:06 +0200

    Use regreet and playmouth

--- modules/nixos/roles/desktop/hyprland.nix
+++ modules/nixos/roles/desktop/hyprland.nix
@@ -6,26 +6,87 @@
 }:
 let
   cfg = config.hostConfig.roles.desktop;
+  catppuccinFlavor = "mocha";
+  catppuccinAccent = "mauve";
+  cursorTheme = {
+    name = "catppuccin-${catppuccinFlavor}-${catppuccinAccent}-cursors";
+    package = pkgs.catppuccin-cursors.mochaMauve;
+  };
+  gtkTheme = {
+    name = "Catppuccin-GTK-Mauve-Dark";
+    package = pkgs.magnetic-catppuccin-gtk.override {
+      tweaks = [ "black" ];
+      accent = [ catppuccinAccent ];
+    };
+  };
 in
 {
   config = lib.mkIf cfg {
-    programs.hyprland = {
+    boot.plymouth.enable = true;
+
+    catppuccin = {
       enable = true;
-      withUWSM = false;
+      autoEnable = true;
+      flavor = catppuccinFlavor;
+      accent = catppuccinAccent;
+
+      cursors.enable = true;
+      plymouth.enable = true;
+      tty.enable = true;
     };
 
-    programs.niri.enable = true;
+    programs = {
+      hyprland = {
+        enable = true;
+        withUWSM = false;
+      };
+
+      niri.enable = true;
+
+      regreet = {
+        enable = true;
+
+        theme = gtkTheme;
+
+        font = {
+          name = "CaskaydiaCove Nerd Font";
+          package = pkgs.nerd-fonts.caskaydia-cove;
+          size = 15;
+        };
+
+        inherit cursorTheme;
+
+        settings = {
+          background = {
+            path = ../../../../extra/wallpapers/minimal-space.jpg;
+            fit = "Cover";
+          };
+
+          GTK.application_prefer_dark_theme = true;
+        };
+
+        extraCss = ''
+          window {
+            background-color: rgba(17, 17, 27, 0.92);
+          }
+
+          box#body {
+            background-color: rgba(24, 24, 37, 0.78);
+            border: 1px solid rgba(203, 166, 247, 0.35);
+            border-radius: 12px;
+            padding: 32px;
+          }
+
+          button {
+            border-radius: 8px;
+          }
+        '';
+      };
+    };
 
     services.greetd = {
       enable = true;
-
-      settings.default_session.command = ''
-        ${pkgs.tuigreet}/bin/tuigreet \
-          --time \
-          --remember \
-          --theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red' \
-          --cmd start-hyprland
-      '';
+      greeterManagesPlymouth = true;
     };
   };
 }
