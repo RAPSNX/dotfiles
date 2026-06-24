a9d8c9dc8bd16f7bc758c35e6c1954a0e1238167
RAPSNX <mail@rapsn.me>
2026-06-24 20:10:45 +0200

    refactor(home-cli): inline language module fragments

--- modules/home/cli/langs/default.nix
+++ modules/home/cli/langs/default.nix
@@ -1,6 +1,26 @@
 {
-  imports = [
-    ./go.nix
-    ./rust.nix
-  ];
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+{
+  programs.go = {
+    enable = true;
+    package = pkgs.go;
+    env = {
+      GOPATH = "${config.home.homeDirectory}/go";
+      GOPRIVATE = [
+        "github.com/stackitcloud"
+        "dev.azure.com/*"
+      ];
+    };
+  };
+
+  config = lib.mkIf (!config.roles.work) {
+    home.packages = builtins.attrValues {
+      inherit (pkgs) clang rustup;
+    };
+    home.sessionPath = [ "$HOME/.cargo/bin" ];
+  };
 }
