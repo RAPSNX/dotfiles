de3b6be725da8d5eb9588b27d3a55c424352a708
RAPSNX <mail@rapsn.me>
2026-06-24 20:10:04 +0200

    refactor(home-cli): inline core module fragments

--- modules/home/cli/core/default.nix
+++ modules/home/cli/core/default.nix
@@ -1,6 +1,68 @@
+{ pkgs, ... }:
 {
-  imports = [
-    ./packages.nix
-    ./programs.nix
-  ];
+  home.packages = builtins.attrValues {
+    # Core utility
+    inherit (pkgs)
+      coreutils
+      dnsutils
+      gnumake
+      gnutar
+      gzip
+      unzip
+      gnused
+      gnugrep
+      killall
+      pciutils
+      parallel
+
+      # Inspection
+      htop
+
+      # Network tools
+      inetutils
+      curl
+      wget
+
+      # Network inspection
+      termshark
+      nmap
+      netcat
+      tcpdump
+      iproute2
+
+      # Text processing
+      jq
+      yq-go
+      gawk
+
+      # Find utils
+      fd
+      ripgrep
+
+      # Copy tools
+      rclone
+
+      # SSH / Security
+      openssh
+      libfido2
+      keepassxc
+      sops
+
+      # Clipboard
+      wl-clipboard
+
+      # Monitor / I2C com
+      ddcutil
+      ;
+  };
+
+  programs = {
+    bat.enable = true;
+    fzf.enable = true;
+    btop.enable = true;
+
+    zoxide.enable = true;
+    neonix.enable = true;
+    eza.enable = true;
+  };
 }
