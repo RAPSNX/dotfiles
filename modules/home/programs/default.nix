157e340b2ca87332cd98b2d21dbc1782c3c822bb
RAPSNX <mail@rapsn.me>
2026-06-24 20:09:20 +0200

    refactor(home): inline trivial wrapper modules

--- modules/home/programs/default.nix
+++ modules/home/programs/default.nix
@@ -1,5 +1,27 @@
+{ pkgs, ... }:
 {
-  imports = [
-    ./guis
-  ];
+  home.packages = builtins.attrValues {
+    # Audio
+    inherit (pkgs)
+      pavucontrol
+      spek
+      vlc
+
+      # Screenshot / Recording
+      grimblast
+      wf-recorder
+
+      # Tools
+      nwg-displays
+      nwg-look
+      gparted
+      gnome-disk-utility
+
+      # Explorer
+      thunar
+
+      # Note taking
+      obsidian
+      ;
+  };
 }
