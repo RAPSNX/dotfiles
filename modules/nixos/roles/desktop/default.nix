f01e80c74d171b1e5e36dde84acb537a13c94650
RAPSNX <mail@rapsn.me>
2026-06-24 15:54:48 +0200

    keyring shit

--- modules/nixos/roles/desktop/default.nix
+++ modules/nixos/roles/desktop/default.nix
@@ -1,8 +1,25 @@
 {
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.hostConfig.roles.desktop;
+in
+{
   imports = [
     ./hyprland.nix
     ./fonts.nix
     ./power.nix
     ./explorer.nix
   ];
+
+  config = lib.mkIf cfg {
+    services.gnome.gnome-keyring.enable = true;
+
+    security.pam.services = {
+      greetd.enableGnomeKeyring = true;
+      login.enableGnomeKeyring = true;
+    };
+  };
 }
