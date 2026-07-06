ba6bc2666d1e64cc52695b90287ad541a46898bb
RAPSNX <github@rapsn.me>
2026-07-06 22:42:57 +0200

    Disable sops!

--- modules/home/services/sops.nix
+++ modules/home/services/sops.nix
@@ -12,31 +12,32 @@ lib.mkIf (config ? targets && config.targets ? genericLinux && config.targets.ge
 
     defaultSopsFile = ../../../secrets/common/ssh.yaml;
 
-    secrets = {
-      ssh_config = {
-        path = "${config.home.homeDirectory}/.ssh/config";
-        mode = "0600";
-      };
-
-      swiss = {
-        path = "${config.home.homeDirectory}/.ssh/swiss";
-        mode = "0600";
-      };
-
-      "swiss.pub" = {
-        path = "${config.home.homeDirectory}/.ssh/swiss.pub";
-        mode = "0644";
-      };
-
-      yubi = {
-        path = "${config.home.homeDirectory}/.ssh/yubi";
-        mode = "0600";
-      };
-
-      "yubi.pub" = {
-        path = "${config.home.homeDirectory}/.ssh/yubi.pub";
-        mode = "0644";
-      };
-    };
+    # TODO: Use this on firefly when adding it to the key
+    # secrets = {
+    #   ssh_config = {
+    #     path = "${config.home.homeDirectory}/.ssh/config";
+    #     mode = "0600";
+    #   };
+    #
+    #   swiss = {
+    #     path = "${config.home.homeDirectory}/.ssh/swiss";
+    #     mode = "0600";
+    #   };
+    #
+    #   "swiss.pub" = {
+    #     path = "${config.home.homeDirectory}/.ssh/swiss.pub";
+    #     mode = "0644";
+    #   };
+    #
+    #   yubi = {
+    #     path = "${config.home.homeDirectory}/.ssh/yubi";
+    #     mode = "0600";
+    #   };
+    #
+    #   "yubi.pub" = {
+    #     path = "${config.home.homeDirectory}/.ssh/yubi.pub";
+    #     mode = "0644";
+    #   };
+    # };
   };
 }
