1a0069e71f5522c2ef74bc9df32b0d2d362eb2c7
RAPSNX <github@rapsn.me>
2026-08-25 23:40:37 +0200

    fix lazygit

--- modules/home/cli/lazygit/default.nix
+++ modules/home/cli/lazygit/default.nix
@@ -3,6 +3,9 @@
   programs.lazygit = {
     enable = true;
     settings = {
+      os = {
+        openLink = "xdg-open {{link}} >/dev/null 2>&1 &";
+      };
       git = {
         autoFetch = true;
         diffRenderers = [
