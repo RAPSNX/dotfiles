9834874ebfe914a247e1b758c16d31c288823d63
RAPSNX <github@rapsn.me>
2026-08-29 21:36:30 +0200

    fix steam / gaming

--- hosts/zion/home.nix
+++ hosts/zion/home.nix
@@ -11,11 +11,12 @@
     stateVersion = lib.mkDefault "22.05";
   };
 
+  # TODO: Check if this is needed
   # UMU uses Steam's Proton 11+ runtime instead of maintaining a second copy.
-  xdg.dataFile."umu/steamrt4" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.local/share/Steam/steamapps/common/SteamLinuxRuntime_4";
-    force = true;
-  };
+  # xdg.dataFile."umu/steamrt4" = {
+  #   source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.local/share/Steam/steamapps/common/SteamLinuxRuntime_4";
+  #   force = true;
+  # };
 
   roles = {
     work = false;
